8c649b8d0ace8dd2a3ee0c33f4e22c5988ed9deb
Jairo <jairosxd1992@gmail.com>
2017-03-27 09:30:06 +0200

    match json body

--- test/UserNotloggedSpec.scala
+++ test/UserNotloggedSpec.scala
@@ -1,7 +1,12 @@
 
 import org.scalatestplus.play._
+
+import play.api.mvc.{Action, EssentialAction}
+import play.api.mvc.Results._
+import play.api.mvc._
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json._
 
 class UserNotloggedSpec extends PlaySpec with OneAppPerTest
 {
@@ -11,6 +16,13 @@ class UserNotloggedSpec extends PlaySpec with OneAppPerTest
 
       status(home) mustBe OK
       contentType(home) mustBe Some("application/json")
+      val action: EssentialAction = Action { request =>
+        val value = (request.body.asJson.get \ "current_path").as[String]
+        Ok(value)
+      }
+      val home_content: JsValue = contentAsJson(home)
+      val home_spec: JsValue = Json.toJson("""{"current_path": "home"}""")
+      home_content mustBe home_spec
     }
   }
 }
