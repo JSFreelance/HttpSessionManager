a74a49d73cf7ae80e513f92d757e295fad3cf5df
Jairo <jairosxd1992@gmail.com>
2017-03-27 09:31:51 +0200

    test refactor

--- test/UserNotloggedSpec.scala
+++ test/UserNotloggedSpec.scala
@@ -1,9 +1,6 @@
 
 import org.scalatestplus.play._
 
-import play.api.mvc.{Action, EssentialAction}
-import play.api.mvc.Results._
-import play.api.mvc._
 import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.json._
@@ -16,10 +13,7 @@ class UserNotloggedSpec extends PlaySpec with OneAppPerTest
 
       status(home) mustBe OK
       contentType(home) mustBe Some("application/json")
-      val action: EssentialAction = Action { request =>
-        val value = (request.body.asJson.get \ "current_path").as[String]
-        Ok(value)
-      }
+
       val home_content: JsValue = contentAsJson(home)
       val home_spec: JsValue = Json.toJson("""{"current_path": "home"}""")
       home_content mustBe home_spec
